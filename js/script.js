3302838004f12932c4ed92a09fda16c4be3adbf5
GindullinRuslan <rusyazlatik@mail.ru>
2022-03-20 15:18:16 +0300

    modal1

--- js/script.js
+++ js/script.js
@@ -73,7 +73,7 @@ document.addEventListener('DOMContentLoaded', () => {
       timeInterval = setInterval(updateClock, 1000);
 
     updateClock();
- 
+
     function updateClock() {
       const t = getTimeRemaining(endtime);
 
@@ -89,4 +89,58 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   setClock('.timer', deadline);
+
+  // Modal
+
+
+  const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalClose = document.querySelector('[data-close]');
+
+    // функция открытия модального окна
+  function openModal() {
+    modal.classList.add('show');
+    modalClose.classList.remove('hide');
+    document.body.style.overflow = 'hidden';
+    // если пользователь открыл окно до вызова сеттаймаут, то не открывать через 3 секунды модальное окно
+    clearTimeout(modalTimer);
+  }
+
+  //при нажатии на триггер срабатывает функция открытия модального окна
+  modalTrigger.forEach(btn => {
+    btn.addEventListener('click', openModal);
+  });
+
+  // функция закрытия модального окна
+  function closeModal() {
+    modal.classList.contains('hide');
+    modal.classList.remove('show');
+    document.body.style.overflow = '';
+  }
+
+  // при нажатии на крестик модалка закроется
+  modalClose.addEventListener('click', closeModal);
+
+  // если пользователь нажмет на область за пределами модалки, то модальное окно закроется
+  modal.addEventListener('click', (e) => {
+    if (e.target === modal && modal.classList.contains('show')) {
+      closeModal();
+    }
+  });
+
+  // закрытие модалки при нажатии на ESC
+  document.addEventListener('keydown', (e) => {
+    if (e.code === 'Escape') {
+      closeModal();
+    }
+  });
+
+  //открытие модального окна через 3 секунды
+  const modalTimer = setTimeout(openModal, 3000);
+
+
+  window.addEventListener('scroll', () => {
+    if (window.pageYOffset + document.documentElement.clientHeight >= document.scrollingElement.scroll)
+  });
+
 });
